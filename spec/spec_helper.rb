2c3f18bc772c123db1e6622aec18baf034af2dc3
Néville Dubuis <neville.dubuis@liquid-concept.ch>
2012-05-16 11:57:37 +0200

    setup google config for spec in untracked file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,14 +6,14 @@ require 'rspec'
 # SimpleCov
 if ENV['COVERAGE']
   require 'simplecov'
-  SimpleCov.start 
+  SimpleCov.start
 end
 
 require 'gcal_mapper'
 
 Spork.prefork do
-  
-  #vcr 
+
+  #vcr
   require 'vcr'
   VCR.configure do |c|
     c.cassette_library_dir = 'spec/vcr'
@@ -24,23 +24,24 @@ Spork.prefork do
       allow_playback_repeats: true
     }
   end
-  
+
   RSpec.configure do |config|
     config.filter_run :focus => true
     config.treat_symbols_as_metadata_keys_with_true_values = true
     config.run_all_when_everything_filtered = true
     config.before :all do
-      @p12 = 'spec/file/privatekey.p12'
-      @client_email = '491507701942-7bc0pf4187fooffdqp6ao1a0b81v6aj2@developer.gserviceaccount.com'
-      @user_email = 'neville.dubuis@liquid-concept.ch'
-      @yaml = 'spec/file/.google-api.yaml'
-      @yaml_relative = '~/.google-api.yaml'
-      @bad_yaml = 'spec/file/bad_yaml.yaml'
-      @calendar_id = 'neville.dubuis@liquid-concept.ch'
+      config = YAML.load_file('spec/file/config.yaml')
+      @p12 = config['p12']
+      @client_email = config['client_email']
+      @user_email = config['user_email']
+      @yaml = config['yaml']
+      @yaml_relative = config['yaml_relative']
+      @bad_yaml = config['bad_yaml']
+      @calendar_id = config['calendar_id']
     end
   end
 
-  
+
 end
 
 Spork.each_run do
